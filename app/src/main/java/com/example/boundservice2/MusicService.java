35d22dfc820c33c67ee4f16909605188d43de79a
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-17 17:18:16 +0700

    create file mp3

--- app/src/main/java/com/example/boundservice2/MusicService.java
+++ app/src/main/java/com/example/boundservice2/MusicService.java
@@ -1,12 +1,42 @@
 package com.example.boundservice2;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.security.PublicKey;
+
 public class MusicService extends Service {
+
+
+    public static final int MSG_PLAY_MUSIC = 1;
+
+    public class myHandler extends Handler {
+        private Context applicationContext;
+
+        public myHandler(Context context) {
+            this.applicationContext = context.getApplicationContext();
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case MSG_PLAY_MUSIC:
+                    break;
+
+                default:
+                    super.handleMessage(msg);
+            }
+            super.handleMessage(msg);
+        }
+    }
+
 
     @Nullable
     @Override
